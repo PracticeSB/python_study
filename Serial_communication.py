f5a137a83d36a33deb7086a6596e624a902fe53f
PracticeSB <suny1357913@naver.com>
2022-12-04 01:27:08 +0900

    Practice

--- Serial_communication.py
+++ Serial_communication.py
@@ -3,7 +3,7 @@ import threading
 import time
 
 #Buffer for memorizing sensor data
-Buffer = []
+Data = []
 
 class Laser():
     def __init__(self,com,baudrate):
@@ -11,16 +11,32 @@ class Laser():
     def SendData(self):
         packet = bytes(bytearray([0x02,0x43,0xB0,0x01,0x03,0xF2]))
         self.mySerial.write(packet)
-        print('Send :' + str(packet),'\n')
+        print('Send :' + str(packet))
     def Setup(self,dataPacket):
         packet = bytes(bytearray(dataPacket))
         self.mySerial.write(packet)
         print('Send :' + str(packet))
     def RecvData(self):
+        global Buffer
         if self.mySerial.readable():
             A = bytearray(self.mySerial.readline())
-            print(A)
-
+            if len(A) < 6:
+                B = self.mySerial.read(size=6-len(A))
+                A.extend(B)
+                print('Additional' + str(A) , len(A))
+            #### 만약에 6보다 긴 경우가 많이 나오면 수정해야할 부분
+            # if len(A) > 6:
+            #     for i in range(len(A) - 1):
+            #         if A[i] == bytes(0x02):
+            #             for count in range(4):
+            #                 Data.append(A[i+count])
+            #                 print('Over 6' + Data)
+            if A[2] >= 127:
+                Response = -((255 - (A[2])) * 256 + (255 - A[3])) * 0.01
+            else:
+                Response = ((A[2]) * 256 + (A[3])) * 0.01
+            print(A,len(A))
+            print('Response: ', Response ,'\n')
 
 
 if __name__=='__main__':
@@ -32,4 +48,4 @@ if __name__=='__main__':
         t1.start()
         t2 = threading.Thread(target=A.RecvData)
         t2.start()
-        time.sleep(0.1)
+        time.sleep(0.01)
