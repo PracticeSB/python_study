2d96d1f2d081b86dfbd7911678c37c22fec19b3f
PracticeSB <suny1357913@naver.com>
2022-12-03 20:59:12 +0900

    Practice

--- Serial_communication.py
+++ Serial_communication.py
@@ -1,6 +1,6 @@
 import serial
 import threading
-class Serialtest():
+class Laser():
     def __init__(self,com,baudrate):
         self.mySerial = serial.Serial(com,baudrate,timeout = 0)
     def SendData(self,dataPacket):
@@ -12,12 +12,15 @@ class Serialtest():
         self.mySerial.write(packet)
         print('Send :' + str(packet))
     def RecvData(self):
-
         packet = self.mySerial.read()
+        print('Receive :' + str(packet))
+        return packet
     def run(self):
         while True:
             self.RecvData()
-port = "COM3"
-baud = 115200
-timeout = 1
-mySerial = serial.Serial = (port, baud, timeout)
+
+if __name__=='__main__':
+    mySerial = Laser("COM3", 115200)
+    t1 = threading.Thread(target=mySerial.RecvData, args=())
+    while True:
+        mySerial.SendData(adsfsdf)
