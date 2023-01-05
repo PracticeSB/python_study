72abc85bec08ac9b27bc16bf63c10de1cd6462e1
suny1 <suny1357913@naver.com>
2023-01-05 10:59:48 +0900

    power_supply

--- Keithley_2231A-30-3.py
+++
@@ -1,6 +0,0 @@
-import pyvisa
-
-rm = pyvisa.ResourceManager()
-Source_meter = rm.open_resource('ASRL5::INSTR') #USB address
-print(rm.list_opened_resources()) #For checking the address
-print(Source_meter.query("*IDN?")) #For checking the response
